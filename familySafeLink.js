b8e7df74085c193fb89b12c1ec2ed1288e6dd45d
Troy Dorman <troydorman@gmail.com>
2021-12-07 13:02:18 -0700

    installed dependencies

--- familySafeLink.js
+++ familySafeLink.js
@@ -1,3 +1,7 @@
+const validUrl = require("valid-url");
+const isPorn = require("is-porn");
+const psl = require("psl");
+
 exports.familySafeLink = (url) => {
   console.log("this is a message from familySafe.js");
 };
