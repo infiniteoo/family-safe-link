e907f2ef746fe7e9f3f41ab65439e58d64e955d4
Troy Dorman <troydorman@gmail.com>
2021-12-07 13:03:33 -0700

    implemented utils extractHostname and bannedKeywords database

--- familySafeLink.js
+++ familySafeLink.js
@@ -1,6 +1,8 @@
 const validUrl = require("valid-url");
 const isPorn = require("is-porn");
 const psl = require("psl");
+const extractHostname = require("../utils/extractHostname");
+const bannedKeywords = require("../utils/bannedKeywords");
 
 exports.familySafeLink = (url) => {
   console.log("this is a message from familySafe.js");
